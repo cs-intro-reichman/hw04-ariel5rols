cadfb24399dee1d3e97a37a2c7a17e7b41bbee71
ariel5rols <ariel.benavi@runi.ac.il>
2024-01-14 02:53:27 +0200

    first question test2

--- ArrayOps.java
+++ ArrayOps.java
@@ -8,17 +8,17 @@ public class ArrayOps {
     public static int findMissingInt (int [] array) {
         Arrays.sort(array);
         if (array.length == 0) {return -1;}
-        else if (array.length == 1) {System.out.println(array[0]); return -1;}
+        else if (array.length == 1) {return array[0];}
         for (int i = 1; i < array.length; i++) {
             if (array[i] - array[i-1] == 1) {}
-            else {System.out.println(array[i-1] + 1); break;}
+            else {return array[i-1] + 1;}
         }
         return -1;
     }
 
     public static int secondMaxValue(int [] array) {
         if (array.length == 0) {return -1;}
-        else if (array.length == 1) {System.out.println(array[0]); return -1;}
+        else if (array.length == 1) {return array[0];}
         int firstMaxValue = array[0], secondMaxValue = array[0];
         for (int i = 1; i < array.length; i++) {
             if (array[i] > firstMaxValue) {
@@ -30,18 +30,25 @@ public class ArrayOps {
                 secondMaxValue = array[i];
             }
         }
-        System.out.println(secondMaxValue);
-        return 0;
+
+        return secondMaxValue;
     }
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
-        return false;
+        for (int i = 0; i < array1.length; i++) {
+            if (!Arrays.asList(array2).contains(array1[i])) {return false;}
+        }
+        for (int i = 0; i < array2.length; i++) {
+            if (!Arrays.asList(array1).contains(array2[i])) {return false;}
+        }
+        return true;
     }
 
     public static boolean isSorted(int [] array) {
-        // Write your code here:
-        return false;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i-1] < array[i]) {return false;}
+        }
+        return true;
     }
 
 }
