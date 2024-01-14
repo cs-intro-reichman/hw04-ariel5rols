1091595373833825f89fcee97712adedccd8f739
ariel5rols <ariel.benavi@runi.ac.il>
2024-01-14 14:30:29 +0200

    fixes2

--- ArrayOps.java
+++ ArrayOps.java
@@ -8,12 +8,12 @@ public class ArrayOps {
     public static int findMissingInt (int [] array) {
         Arrays.sort(array);
         if (array.length == 0) {return -1;}
-        else if (array.length == 1) {return array[0];}
+        else if (array.length == 1) {return array[0]+1;}
         for (int i = 1; i < array.length; i++) {
             if (array[i] - array[i-1] == 1) {}
             else {return array[i-1] + 1;}
         }
-        return -1;
+        return array[0]-1;
     }
 
     public static int secondMaxValue(int [] array) {
@@ -21,14 +21,16 @@ public class ArrayOps {
         else if (array.length == 1) {return array[0];}
         int firstMaxValue = array[0], secondMaxValue = array[0];
         for (int i = 1; i < array.length; i++) {
+            if (array[i] > secondMaxValue) {
+                secondMaxValue = array[i];
+            }
+
             if (array[i] > firstMaxValue) {
                 secondMaxValue = firstMaxValue;
                 firstMaxValue = array[i];
                 
             }
-            if (array[i] > secondMaxValue) {
-                secondMaxValue = array[i];
-            }
+
         }
 
         return secondMaxValue;
@@ -46,7 +48,7 @@ public class ArrayOps {
 
     public static boolean isSorted(int [] array) {
         for (int i = 1; i < array.length; i++) {
-            if (array[i-1] < array[i]) {return false;}
+            if (array[i] < array[i-1]) {return false;}
         }
         return true;
     }
